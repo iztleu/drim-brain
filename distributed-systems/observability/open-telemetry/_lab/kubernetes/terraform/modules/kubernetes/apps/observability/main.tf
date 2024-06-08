0801f8dfd068a7bd2ec088b979729d4e84d9159d
mitro <dmtr.melnik@gmail.com>
2024-06-08 12:34:49 +0500

    #open-telemetry: debug otel collector daemonsets

--- distributed-systems/observability/open-telemetry/_lab/kubernetes/terraform/modules/kubernetes/apps/observability/main.tf
+++ distributed-systems/observability/open-telemetry/_lab/kubernetes/terraform/modules/kubernetes/apps/observability/main.tf
@@ -10,6 +10,7 @@ locals {
   grafana_cert = "grafana-cert"
   prometheus_cert = "prometheus-cert"
   alertmanager_cert = "alertmanager-cert"
+  mimir_tenant = "otel-lab"
 }
 
 resource "random_string" "grafana_admin_password" {
@@ -28,14 +29,14 @@ resource "kubernetes_secret" "grafana_admin_password" {
 }
 
 resource "helm_release" "prometheus-stack" {
-  name       = "prometheus-stack"
+  name = "prometheus-stack"
   repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "kube-prometheus-stack"
-  version    = "59.0.0"
-  namespace  = kubernetes_namespace.observability.metadata[0].name
+  chart = "kube-prometheus-stack"
+  version = "59.0.0"
+  namespace = kubernetes_namespace.observability.metadata[0].name
 
   values = [
-    templatefile("${path.module}/helm/prometheus-stack/values.yaml.tpl", {
+    templatefile("${path.module}/prometheus-stack/values.yaml.tpl", {
       namespace = kubernetes_namespace.observability.metadata[0].name,
       storage_class_name = var.storage_class_name,
       storage_size = var.prometheus_storage_size,
@@ -47,9 +48,9 @@ resource "helm_release" "prometheus-stack" {
 resource "kubernetes_manifest" "grafana_cert" {
   manifest = {
     apiVersion = "cert-manager.io/v1"
-    kind       = "Certificate"
+    kind = "Certificate"
     metadata = {
-      name      = local.grafana_cert
+      name = local.grafana_cert
       namespace = kubernetes_namespace.observability.metadata[0].name
     }
     spec = {
@@ -69,9 +70,9 @@ resource "kubernetes_manifest" "grafana_cert" {
 resource "kubernetes_manifest" "grafana_ingressroute" {
   manifest = {
     apiVersion = "traefik.io/v1alpha1"
-    kind       = "IngressRoute"
+    kind = "IngressRoute"
     metadata = {
-      name      = "grafana"
+      name = "grafana"
       namespace = kubernetes_namespace.observability.metadata[0].name
     }
     spec = {
@@ -97,65 +98,12 @@ resource "kubernetes_manifest" "grafana_ingressroute" {
   }
 }
 
-# resource "kubernetes_manifest" "prometheus_cert" {
-#   manifest = {
-#     apiVersion = "cert-manager.io/v1"
-#     kind       = "Certificate"
-#     metadata = {
-#       name      = local.prometheus_cert
-#       namespace = kubernetes_namespace.observability.metadata[0].name
-#     }
-#     spec = {
-#       commonName = var.prometheus_url
-#       secretName = local.prometheus_cert
-#       dnsNames = [
-#         var.prometheus_url
-#       ]
-#       issuerRef = {
-#         kind = "ClusterIssuer"
-#         name = var.cert_manager_cluster_issuer
-#       }
-#     }
-#   }
-# }
-
-# resource "kubernetes_manifest" "prometheus_ingressroute" {
-#   manifest = {
-#     apiVersion = "traefik.io/v1alpha1"
-#     kind       = "IngressRoute"
-#     metadata = {
-#       name      = "prometheus"
-#       namespace = kubernetes_namespace.observability.metadata[0].name
-#     }
-#     spec = {
-#       entryPoints = [
-#         var.traefik_entry_point
-#       ]
-#       routes = [
-#         {
-#           match = "Host(`${var.prometheus_url}`)"
-#           kind  = "Rule"
-#           services = [
-#             {
-#               name = "prometheus-stack-kube-prom-prometheus"
-#               port = 9090
-#             }
-#           ]
-#         }
-#       ]
-#       tls = {
-#         secretName = local.prometheus_cert
-#       }
-#     }
-#   }
-# }
-
 resource "kubernetes_manifest" "alertmanager_cert" {
   manifest = {
     apiVersion = "cert-manager.io/v1"
-    kind       = "Certificate"
+    kind = "Certificate"
     metadata = {
-      name      = local.alertmanager_cert
+      name = local.alertmanager_cert
       namespace = kubernetes_namespace.observability.metadata[0].name
     }
     spec = {
@@ -175,9 +123,9 @@ resource "kubernetes_manifest" "alertmanager_cert" {
 resource "kubernetes_manifest" "alertmanager_ingressroute" {
   manifest = {
     apiVersion = "traefik.io/v1alpha1"
-    kind       = "IngressRoute"
+    kind = "IngressRoute"
     metadata = {
-      name      = "alertmanager"
+      name = "alertmanager"
       namespace = kubernetes_namespace.observability.metadata[0].name
     }
     spec = {
@@ -206,14 +154,14 @@ resource "kubernetes_manifest" "alertmanager_ingressroute" {
 # Loki
 
 resource "helm_release" "loki" {
-  name       = "loki"
+  name = "loki"
   repository = "https://grafana.github.io/helm-charts"
-  chart      = "loki"
-  version    = "6.6.2"
-  namespace  = kubernetes_namespace.observability.metadata[0].name
+  chart = "loki"
+  version = "6.6.2"
+  namespace = kubernetes_namespace.observability.metadata[0].name
 
   values = [
-    templatefile("${path.module}/helm/loki/values.yaml.tpl", {
+    templatefile("${path.module}/loki/values.yaml.tpl", {
       s3_endpoint = var.s3_endpoint,
       s3_access_key_id = var.s3_access_key_id,
       s3_secret_access_key = var.s3_secret_access_key,
@@ -249,14 +197,14 @@ resource "kubernetes_config_map" "grafana_loki_datasource" {
 # Tempo
 
 resource "helm_release" "tempo" {
-  name       = "tempo"
+  name = "tempo"
   repository = "https://grafana.github.io/helm-charts"
-  chart      = "tempo-distributed"
-  version    = "1.9.11"
-  namespace  = kubernetes_namespace.observability.metadata[0].name
+  chart = "tempo-distributed"
+  version = "1.9.11"
+  namespace = kubernetes_namespace.observability.metadata[0].name
 
   values = [
-    templatefile("${path.module}/helm/tempo/values.yaml.tpl", {
+    templatefile("${path.module}/tempo/values.yaml.tpl", {
       s3_endpoint = var.s3_endpoint,
       s3_access_key_id = var.s3_access_key_id,
       s3_secret_access_key = var.s3_secret_access_key,
@@ -266,7 +214,7 @@ resource "helm_release" "tempo" {
 
 resource "kubernetes_config_map" "grafana_tempo_datasource" {
   metadata {
-    name      = "grafana-tempo-datasource"
+    name = "grafana-tempo-datasource"
     namespace = kubernetes_namespace.observability.metadata[0].name
     labels = {
       grafana_datasource = "1"
@@ -291,14 +239,14 @@ resource "kubernetes_config_map" "grafana_tempo_datasource" {
 # Mimir
 
 resource "helm_release" "mimir" {
-  name       = "mimir"
+  name = "mimir"
   repository = "https://grafana.github.io/helm-charts"
-  chart      = "mimir-distributed"
-  version    = "5.3.0"
+  chart = "mimir-distributed"
+  version = "5.3.0"
   namespace  = kubernetes_namespace.observability.metadata[0].name
 
   values = [
-    templatefile("${path.module}/helm/mimir/values.yaml.tpl", {
+    templatefile("${path.module}/mimir/values.yaml.tpl", {
       s3_endpoint = var.s3_endpoint,
       s3_access_key_id = var.s3_access_key_id,
       s3_secret_access_key = var.s3_secret_access_key,
@@ -308,7 +256,7 @@ resource "helm_release" "mimir" {
 
 resource "kubernetes_config_map" "grafana_mimir_datasource" {
   metadata {
-    name      = "grafana-mimir-datasource"
+    name = "grafana-mimir-datasource"
     namespace = kubernetes_namespace.observability.metadata[0].name
     labels = {
       grafana_datasource = "1"
@@ -328,41 +276,180 @@ resource "kubernetes_config_map" "grafana_mimir_datasource" {
             httpMethod: POST
             httpHeaderName1: X-Scope-OrgID
           secureJsonData:
-            httpHeaderValue1: grafana
+            httpHeaderValue1: ${local.mimir_tenant}
       EOT
+  }
+}
+
+# OpenTelemetry Operator
+
+resource "helm_release" "open_telemetry_operator" {
+  name = "open-telemetry-operator"
+  repository = "https://open-telemetry.github.io/opentelemetry-helm-charts"
+  chart = "opentelemetry-operator"
+  version = "0.61.0"
+  namespace = kubernetes_namespace.observability.metadata[0].name
+
+  set {
+    name = "manager.collectorImage.repository"
+    value = "otel/opentelemetry-collector-contrib"
   }
 }
 
 # OpenTelemetry Collector DaemonSet
 
-resource "helm_release" "open-telemetry-collector-daemonset" {
-  name       = "open-telemetry-collector-daemonset"
-  repository = "https://open-telemetry.github.io/opentelemetry-helm-charts"
-  chart      = "opentelemetry-collector"
-  version    = "0.92.0"
-  namespace  = kubernetes_namespace.observability.metadata[0].name
+resource "kubernetes_manifest" "open_telemetry_collector_daemonset" {
+  computed_fields = ["spec.config"]
+  manifest = {
+    apiVersion = "opentelemetry.io/v1alpha1"
+    kind = "OpenTelemetryCollector"
+    metadata = {
+      name = "open-telemetry-collector-daemonset"
+      namespace = kubernetes_namespace.observability.metadata[0].name
+    }
+    spec = {
+      mode = "daemonset"
+      targetAllocator = {
+        enabled = true
+        allocationStrategy = "per-node"
+        prometheusCR = {
+          enabled = true
+        }
+      }
+      env = [
+        {
+          name = "K8S_NODE_IP"
+          valueFrom = {
+            fieldRef = {
+              fieldPath = "status.hostIP"
+            }
+          }
+        }
+      ]
+      config = templatefile("${path.module}/open-telemetry-collector/daemonset.config.yaml.tpl", {
+        tempo_endpoint = "http://tempo-distributor:4317",
+        prometheus_remote_write_endpoint = "http://mimir-distributor:8080/api/v1/push",
+        prometheus_remote_write_org_id = local.mimir_tenant,
+      })
+    }
+  }
+}
 
-  values = [
-    templatefile("${path.module}/helm/open-telemetry-collector/daemonset.values.yaml.tpl", {
-      loki_endpoint = "http://loki-write:3100/loki/api/v1/push",
-      tempo_endpoint = "http://tempo-distributor:4317",
-    })
-  ]
+## RBAC configs for DaemonSet Collector
+
+resource "kubernetes_cluster_role" "open_telemetry_collector_daemonset_role" {
+  metadata {
+    name = "open-telemetry-collector-daemonset-role"
+  }
+  rule {
+    api_groups = [""]
+    resources = ["nodes/stats"]
+    verbs = ["get", "list", "watch"]
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "open_telemetry_collector_daemonset_role_binding" {
+  metadata {
+    name = "open-telemetry-collector-daemonset-role-binding"
+  }
+
+  subject {
+    kind = "ServiceAccount"
+    name = "open-telemetry-collector-daemonset-collector"
+    namespace = kubernetes_namespace.observability.metadata[0].name
+  }
+
+  role_ref {
+    kind = "ClusterRole"
+    name = kubernetes_cluster_role.open_telemetry_collector_daemonset_role.metadata[0].name
+    api_group = "rbac.authorization.k8s.io"
+  }
+}
+
+## RBAC configs for Target Allocator
+
+resource "kubernetes_cluster_role" "open_telemetry_target_allocator_role" {
+  metadata {
+    name = "open-telemetry-target-allocator-role"
+  }
+
+  rule {
+    api_groups = [""]
+    resources = ["nodes", "nodes/metrics", "services", "endpoints", "pods"]
+    verbs = ["get", "list", "watch"]
+  }
+
+  rule {
+    api_groups = [""]
+    resources = ["configmaps"]
+    verbs = ["get"]
+  }
+
+  rule {
+    api_groups = ["discovery.k8s.io"]
+    resources = ["endpointslices"]
+    verbs = ["get", "list", "watch"]
+  }
+
+  rule {
+    api_groups = ["networking.k8s.io"]
+    resources = ["ingresses"]
+    verbs = ["get", "list", "watch"]
+  }
+
+  rule {
+    non_resource_urls = ["/metrics"]
+    verbs = ["get"]
+  }
+
+  rule {
+    api_groups = ["monitoring.coreos.com"]
+    resources = ["servicemonitors", "podmonitors"]
+    verbs = ["*"]
+  }
+
+  rule {
+    api_groups = [""]
+    resources = ["namespaces"]
+    verbs = ["get", "list", "watch"]
+  }
+}
+
+resource "kubernetes_cluster_role_binding" "open_telemetry_target_allocator_cluster_role_binding" {
+  metadata {
+    name = "open-telemetry-target-allocator-cluster-role-binding"
+  }
+
+  subject {
+    kind = "ServiceAccount"
+    name = "open-telemetry-collector-daemonset-targetallocator"
+    namespace = kubernetes_namespace.observability.metadata[0].name
+  }
+
+  role_ref {
+    kind = "ClusterRole"
+    name = kubernetes_cluster_role.open_telemetry_target_allocator_role.metadata[0].name
+    api_group = "rbac.authorization.k8s.io"
+  }
 }
 
 # OpenTelemetry Collector Deployment
 
-resource "helm_release" "open-telemetry-collector-deployment" {
-  name       = "open-telemetry-collector-deployment"
-  repository = "https://open-telemetry.github.io/opentelemetry-helm-charts"
-  chart      = "opentelemetry-collector"
-  version    = "0.92.0"
-  namespace  = kubernetes_namespace.observability.metadata[0].name
-
-  values = [
-    templatefile("${path.module}/helm/open-telemetry-collector/deployment.values.yaml.tpl", {
-      loki_endpoint = "http://loki-write:3100/loki/api/v1/push",
-      tempo_endpoint = "http://tempo-distributor:4317",
-    })
-  ]
-}
+# resource "kubernetes_manifest" "open-telemetry-collector-deployment" {
+#   computed_fields = ["spec.config"]
+#   manifest = {
+#     apiVersion = "opentelemetry.io/v1alpha1"
+#     kind = "OpenTelemetryCollector"
+#     metadata = {
+#       name = "open-telemetry-collector-deployment"
+#       namespace = kubernetes_namespace.observability.metadata[0].name
+#     }
+#     spec = {
+#       mode = "deployment"
+#       config = templatefile("${path.module}/open-telemetry-collector/deployment.config.yaml.tpl", {
+#         prometheus_remote_write_endpoint = "http://mimir-distributor:8080/api/v1/push",
+#         prometheus_remote_write_org_id = local.mimir_tenant,
+#       })
+#     }
+#   }
+# }
---
+++ distributed-systems/observability/open-telemetry/_lab/kubernetes/terraform/modules/kubernetes/apps/observability/open-telemetry-collector/daemonset.config.yaml.tpl
@@ -0,0 +1,60 @@
+receivers:
+  otlp:
+    protocols:
+      grpc:
+        endpoint: "0.0.0.0:4317"
+      http:
+        endpoint: "0.0.0.0:4318"
+  prometheus:
+    config:
+      scrape_configs:
+      - job_name: otel-collector
+        scrape_interval: 10s
+        static_configs:
+        - targets: [ "0.0.0.0:8888" ]
+        metric_relabel_configs:
+        - action: labeldrop
+          regex: (id|name)
+          replacement: $$1
+        - action: labelmap
+          regex: label_(.+)
+          replacement: $$1
+  kubeletstats:
+    collection_interval: 10s
+    auth_type: "serviceAccount"
+    endpoint: "https://$${env:K8S_NODE_IP}:10250"
+    insecure_skip_verify: true
+    metric_groups:
+      - pod
+      - container
+
+processors:
+  batch:
+
+exporters:
+  otlp:
+    endpoint: ${tempo_endpoint}
+    tls:
+      insecure: true
+  prometheusremotewrite:
+    endpoint: ${prometheus_remote_write_endpoint}
+    headers:
+      "X-Scope-OrgID": ${prometheus_remote_write_org_id}
+
+service:
+  pipelines:
+    traces:
+      receivers:
+      - otlp
+      processors:
+      - batch
+      exporters:
+      - otlp
+    metrics:
+      receivers:
+      - prometheus
+      - kubeletstats
+      processors:
+      - batch
+      exporters:
+      - prometheusremotewrite
